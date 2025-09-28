066e26c26b6e7a0d1df086b106a5e57b1094d173
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-28 16:30:10 +0300

    Fix test description to be in English

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const { analyzeRepository } = require('./index');
 
-describe('Version Control Analyse Tests', function() {
-    it('sollte ein nicht-leeres Repository analysieren', function() {
+describe('Version Control Analyze Tests', function() {
+    it('should analyze a non-empty repository', function() {
         const result = analyzeRepository('/pfad/zum/test-repo'); // Dummy-Path
         assert.notStrictEqual(result, null);
     });
